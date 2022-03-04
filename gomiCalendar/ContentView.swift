4eda505cbbe88c23d0c282d6866d33e115346057
川崎春菜 <moomin0117hq_kmit.t@gmail.com>
2022-03-04 18:11:15 +0900

    Initial Commit

---
+++ gomiCalendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gomiCalendar
+//
+//  Created by 川崎　春菜 on 2022/03/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gomiCalendar/gomiCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  gomiCalendarApp.swift
+//  gomiCalendar
+//
+//  Created by 川崎　春菜 on 2022/03/04.
+//
+
+import SwiftUI
+
+@main
+struct gomiCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
