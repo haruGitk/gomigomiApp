d8f51c22ed4c5c6992b65cac0db0ffef867faf30
doraryu1 <nazenanoyou@yahoo.co.jp>
2022-03-04 22:51:25 +0900

    firebase installation

---
+++ gomiCalendar/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  gomiCalendar
+//
+//  Created by liuchenghui on 2022/03/04.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate{
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
