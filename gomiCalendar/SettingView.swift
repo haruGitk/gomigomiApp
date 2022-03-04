c6ecd279bf6726adb9cf2a1859ee8562cded07bc
masam0 <masaya.esp@gmail.com>
2022-03-04 22:57:08 +0900

    Implement textField and Picker
    地域設定画面の作成をしました。
    現時点では、ボタンは入力された値をprintすることしかできません。

--- gomiCalendar/SettingView.swift
+++ gomiCalendar/SettingView.swift
@@ -37,19 +37,29 @@ struct ModalView: View {
     @State var prefecture: String = ""
     @State var city: String = ""
     @State var region: String = ""
+    @State var block: String = ""
     @State var showingModal: Bool
     
     var body: some View {
         NavigationView {
             VStack(spacing: 30) {
                 VStack(alignment: .center, spacing: 25) {
-                    TextField("都道府県", text: $prefecture)
-                    TextField("市町村", text: $city)
-                    TextField("地区・丁目", text: $region)
+                            Picker(selection: $prefecture, label: Text("都道府県")) {
+                                ForEach(prefecturesData) {
+                                    prefecture in
+                                    Text(prefecture.name).tag(prefecture.name)
+                                }
+                            }
+                            .pickerStyle(.wheel)
+                            .frame(height: 65)
+                            .clipped()
+                    TextField("市区町村", text: $city)
+                    TextField("地区", text: $region)
+                    TextField("丁目", text: $block)
                 }
                 .padding(20)
                 .textFieldStyle(.roundedBorder)
-                Button(action: {print("button pressed!")})
+                Button(action: {print("都道府県: \(prefecture), 市町村: \(city), 地域: \(region)" )})
                     {
                     Text("設定する")
                         .font(.footnote)
